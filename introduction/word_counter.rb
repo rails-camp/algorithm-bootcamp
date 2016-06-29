800a902cf25b9cfea100920779985d3540a4c721
Jordan Hudgens <jordan@wow.com>
2016-06-29 15:36:42 -0500

    Added introduction guide

---
+++ introduction/word_counter.rb
@@ -0,0 +1,17 @@
+# Words stored in a variable
+words = "My amazing document full of words"
+
+# Convert words into an array, output is:
+# ["My", "amazing", "document", "full", "of", "words"]
+words_array = words.split
+
+# Set a counter
+sum = 0
+
+# Loop through the words array
+words_array.each do |word|
+	sum += 1
+end
+
+# Print out the word count
+puts sum
