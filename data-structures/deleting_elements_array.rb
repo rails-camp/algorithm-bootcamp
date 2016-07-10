a25aef52701cf5ee26c3e918276625ef7035c324
Jordan Hudgens <jordan@wow.com>
2016-07-09 22:17:36 -0500

    Deleting items from an array

---
+++ data-structures/deleting_elements_array.rb
@@ -0,0 +1,19 @@
+players_and_positions = [
+  ["George Springer", "OF"],
+  ["Luis Valbuena", "OF"],
+  ["Carlos Correa", "SS"],
+  ["Evan Gattis", "DH"],
+  ["A.J. Reed", "1B"],
+  ["Carlos Gomez", "OF"],
+  ["Jason Castro", "C"]
+]
+
+# players_and_positions.delete_at(0)
+# players_and_positions[4].delete_at(1)
+# players_and_positions.pop
+
+# puts players_and_positions
+
+at_bat = players_and_positions.delete_at(2)
+
+puts at_bat
